3f3c6e4e65b1ca5ee1b99fa3bc2fd21252b4b1e2
Temirlan Almazbek uulu <talmazbekuulu@in.nurtelecom.kg>
2025-11-11 17:45:49 +0600

    рефакторинг

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,224 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var numberLIST: List<String>
+    var mainStr: String = ""
+    var bracketsOpen = false
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        binding.nine.setOnClickListener {
+            InputDigit(mainStr, 9)
+        }
+        binding.eight.setOnClickListener {
+            InputDigit(mainStr, 8)
+        }
+        binding.seven.setOnClickListener {
+            InputDigit(mainStr, 7)
+        }
+        binding.six.setOnClickListener {
+            InputDigit(mainStr, 6)
+        }
+        binding.five.setOnClickListener {
+            InputDigit(mainStr, 5)
+        }
+        binding.four.setOnClickListener {
+            InputDigit(mainStr, 4)
+        }
+        binding.three.setOnClickListener {
+            InputDigit(mainStr, 3)
+        }
+        binding.two.setOnClickListener {
+            InputDigit(mainStr, 2)
+        }
+        binding.one.setOnClickListener {
+            InputDigit(mainStr, 1)
+        }
+        binding.zero.setOnClickListener {
+            InputDigit(mainStr, 0)
+        }
+        binding.AC.setOnClickListener {
+            inputAC()
+
+        }
+
+        binding.delete.setOnClickListener {
+            mainStr = mainStr.dropLast(1)
+            binding.mainInput.setText(mainStr)
+        }
+
+        binding.dot.setOnClickListener {
+            inputDot()
+
+
+        }
+
+        binding.brackets.setOnClickListener {
+            if (inputOpenBracket()) {
+            } else inputCloseBracket()
+        }
+
+        binding.plus.setOnClickListener {
+            addOperationSymbol("+")
+        }
+        binding.minus.setOnClickListener {
+            addOperationSymbol("-")
+        }
+
+        binding.multiplication.setOnClickListener {
+            addOperationSymbol("*")
+        }
+        binding.division.setOnClickListener {
+            addOperationSymbol("/")
+        }
+        binding.percent.setOnClickListener {
+            addOperationSymbol("%")
+        }
+
+
+        binding.equal.setOnClickListener {
+            inputEqual(mainStr)
+        }
+    }
+
+    fun InputDigit(str: String, digit: Int) {
+        if (str.length != 0 && str.last() == ')') {
+            mainStr += "*$digit"
+            binding.mainInput.setText(mainStr)
+        } else {
+            mainStr += "$digit"
+            binding.mainInput.setText(mainStr)
+
+        }
+
+    }
+
+    fun inputEqual(str: String) {
+        try {
+            val str = binding.mainInput.text.toString()
+            val rpn = infixToRPN(str)
+            val result = evaluateRPN(rpn)
+
+            if (result % 1.0 == 0.0) {
+                binding.output.text = "${result.toInt()}"
+            } else {
+                binding.output.text = "${result}"
+            }
+        } catch (e: Exception) {
+            Toast.makeText(this, "Ошибка ввода данных", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+    fun isOperation(lastchar: Char): Boolean {
+        return lastchar == '+' || lastchar == '-' || lastchar == '*' || lastchar == '/' || lastchar == '%'
+    }
+    fun canAddOperation(lastchar: Char): Boolean {
+        return mainStr.length != 0 && (mainStr.last()
+            .isDigit() || mainStr.last() == ')') && mainStr.last() != '('
+    }
+    fun canCloseBracket(str: String): Boolean {
+        if (str == "") return false
+        var counter = 0
+        val listToken: List<String>
+        for (i in str.length - 1 downTo 0) {
+            if (str[i] == '(') {
+                counter = i
+            }
+        }
+
+        print("counter $counter")
+
+        listToken = str.slice(counter + 1 until str.length).split("+", "-", "*", "/", "%", "(", ")")
+        print("list $listToken")
+        return listToken.size > 1
+    }
+    fun addOperationSymbol(op: String) {
+        if (mainStr != "" && canAddOperation(mainStr.last())) {
+            mainStr += op
+            binding.mainInput.setText(mainStr)
+        }
+    }
+    fun inputCloseBracket() {
+        if (mainStr.last().isDigit() && bracketsOpen == true && canCloseBracket(mainStr)) {
+            mainStr += ")"
+            binding.mainInput.setText(mainStr)
+            bracketsOpen = false
+        } else if (mainStr.last() == '.' && bracketsOpen == true) {
+
+            mainStr += "0)"
+            binding.mainInput.setText(mainStr)
+            bracketsOpen = false
+
+        }
+    }
+    fun inputOpenBracket(): Boolean {
+        if (mainStr.length == 0 || isOperation(mainStr.last())
+            && bracketsOpen == false && mainStr.last() != '('
+        ) {
+            mainStr += "("
+            binding.mainInput.setText(mainStr)
+            bracketsOpen = true
+            return true
+
+        } else if ((mainStr.last()
+                .isDigit() || mainStr.last() == ')') && bracketsOpen == false
+        ) {
+            mainStr += "*("
+            binding.mainInput.setText(mainStr)
+            bracketsOpen = true
+            return true
+
+        } else if (mainStr.last() == '.' && bracketsOpen == false) {
+            mainStr += "0*("
+            binding.mainInput.setText(mainStr)
+            bracketsOpen = true
+            return true
+        } else return false
+    }
+
+    fun inputDot() {
+        numberLIST = mainStr.split("+", "-", "*", "/", "%", "(", ")")
+        val lastPart = numberLIST.lastOrNull() ?: ""
+
+        if (mainStr != "" && mainStr.last() != '.' && mainStr.last()
+                .isDigit() && !lastPart.contains(".")
+        ) {
+            mainStr += "."
+            binding.mainInput.setText(mainStr)
+        }
+    }
+
+    fun inputAC() {
+        mainStr = ""
+        binding.mainInput.setText("")
+        binding.output.text = ""
+        bracketsOpen = false
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/myapplication/opn.kt
@@ -0,0 +1,86 @@
+package com.example.myapplication
+
+import java.util.Stack
+
+//преобразование в обратную польскую запись
+fun infixToRPN(expression: String): List<String> {
+    val output = mutableListOf<String>()
+    val stack = Stack<Char>()
+
+    val tokens = tokenize(expression)
+
+    for (token in tokens) {
+        when {
+            token.isDoubleOrInt() -> output.add(token)
+            token.length == 1 && isOperator(token[0]) -> {
+                val op = token[0]
+                while (stack.isNotEmpty() && precedence(stack.peek()) >= precedence(op)) {
+                    output.add(stack.pop().toString())
+                }
+                stack.push(op)
+            }
+            token == "(" -> stack.push('(')
+            token == ")" -> {
+                while (stack.isNotEmpty() && stack.peek() != '(') {
+                    output.add(stack.pop().toString())
+                }
+                if (stack.isNotEmpty() && stack.peek() == '(') stack.pop()
+            }
+        }
+    }
+
+    while (stack.isNotEmpty()) {
+        output.add(stack.pop().toString())
+    }
+
+    return output
+}
+
+//вычислеие выражения
+fun evaluateRPN(rpn: List<String>): Double {
+    val stack = Stack<Double>()
+
+    for (token in rpn) {
+        if (token.isDoubleOrInt()) {
+            stack.push(token.toDouble())
+        } else if (token.length == 1 && isOperator(token[0])) {
+            val b = stack.pop()
+            val a = stack.pop()
+            val result = when (token[0]) {
+                '+' -> a + b
+                '-' -> a - b
+                '×', '*' -> a * b
+                '÷', '/' -> a / b
+                '%' -> a/100.0*b
+                else -> throw IllegalArgumentException("Unknown operator: ${token[0]}")
+            }
+            stack.push(result)
+        }
+    }
+    return  stack.pop()
+
+}
+
+//разделение на элементы
+fun tokenize(expression: String): List<String> {
+    val regex = Regex("([+\\-×÷*/()%])")
+    return expression
+        .replace(" ", "")
+        .replace(regex, " $1 ")
+        .trim()
+        .split(Regex("\\s+"))
+        .filter { it.isNotEmpty() }
+}
+
+// Проверка на оператор
+fun isOperator(c: Char) = c in listOf('+', '-', '×', '÷', '*', '/','%')
+
+// приоритеты
+fun precedence(c: Char): Int = when (c) {
+    '+', '-' -> 1
+    '×', '*', '÷', '/' ,'%'-> 2
+    else -> 0
+}
+
+fun String.isDoubleOrInt(): Boolean = this.toDoubleOrNull() != null
+
